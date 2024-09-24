070af9338d69a72b45b1093d648977c5ef6889b2
Phuc Nguyen <phuc.nguyen.ufo@gmail.com>
2024-09-24 16:31:31 -0400

    Initial Commit

---
+++ SideMenuExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenuExample
+//
+//  Created by Phuc Nguyen on 2024-09-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
